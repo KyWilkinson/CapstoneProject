7664151ca7f3f13b5b84a0cb1dad0b433c186c6a
KyWilkinson <wilki018@hotmail.com>
2020-10-21 15:31:43 -0500

    created veiws for player profile

--- Capstone/Data/ApplicationDbContext.cs
+++ Capstone/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Capstone.Models;
 
 namespace Capstone.Data
 {
@@ -12,5 +13,6 @@ namespace Capstone.Data
             : base(options)
         {
         }
+        public DbSet<Capstone.Models.PlayerProfile> PlayerProfile { get; set; }
     }
 }
