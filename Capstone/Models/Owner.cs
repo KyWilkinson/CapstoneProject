852ada1854a3bb437b7df33f19e9b109abc1ffe5
KyWilkinson <wilki018@hotmail.com>
2020-10-21 14:41:23 -0500

    added and modified properties to owner class

--- Capstone/Models/Owner.cs
+++ Capstone/Models/Owner.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +9,20 @@ namespace Capstone.Models
 {
     public class Owner
     {
-        
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "First Name")]
+        public string firstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string lastName { get; set; }
+
+        [Display(Name = "Team Name")]
+        public string teamOwned { get; set; }
+
+        [ForeignKey("Player Sheet"]
+        public int? playerId { get; set; }
+        public PlayerProfile profile { get; set; }
     }
 }
