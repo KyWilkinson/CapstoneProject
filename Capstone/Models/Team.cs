239b5691ac5380358e0fadcf6ff0a6ad38bbef91
KyWilkinson <wilki018@hotmail.com>
2020-10-21 15:13:10 -0500

    Added foriegn key owner to team model

--- Capstone/Models/Team.cs
+++ Capstone/Models/Team.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,18 @@ namespace Capstone.Models
 {
     public class Team
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Team Name")]
+        public string teamName { get; set; }
+
+        [ForeignKey("Players")]
+        public int? playerId { get; set; }
+        public PlayerProfile playerProfile { get; set; }
+
+        [ForeignKey("Owner")]
+        public int? ownerId { get; set; }
+        public Owner owner { get; set; }
     }
 }
